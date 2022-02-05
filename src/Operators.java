e6a197b307154661d94b611b824c777d556beb22
eslam <eslamfgazy600@gmail.com>
2022-02-05 13:18:10 +0200

    Kotlin

---
+++ src/Operators.java
@@ -0,0 +1,28 @@
+public class Operators {
+
+
+    public static void main(String[] args) {
+
+
+        int x = 25;
+        int c = 15;
+//        x |= 3;
+        System.out.println(x|c);
+        System.out.println(x^c);
+        System.out.println(x&c);
+
+        int z = 4;//100
+        int z1 = 5; //101
+        System.out.println(z<<z1);
+        System.out.println(z1<<z);
+        int ss = 2;
+        System.out.println(z>>ss);
+
+        x |= 15;
+        System.out.println(x);
+
+    }
+
+}
+
+
